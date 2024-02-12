f5e7b038b9db3e6a7c30172760bcf94a9ee18c7f
arelhan <are255gu@gmail.com>
2024-02-13 00:01:47 +0300

    bugünlük bu kadar

--- 03/12_list.py
+++ 03/12_list.py
@@ -1,5 +1,6 @@
 
 userA = ['Ruşen','Elhan',14]
 userB = ['Ahsen','Elhan',13]
-users=userA+userB
-print(users)
+users=[userA,userB]
+#users dizisinin 1. elemanının 0. öğesini ekrana yazdır.
+print(users[1][0])
