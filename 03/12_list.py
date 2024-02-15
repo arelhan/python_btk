3509ff9f7cacb3f53dbb8c5de1c08f4d9c07255e
Ruşen <are255gu@gmail.com>
2024-02-15 10:06:18 +0300

    senk

--- 03/12_list.py
+++ 03/12_list.py
@@ -1,6 +1,8 @@
 
 userA = ['Ruşen','Elhan',14]
-userB = ['Ahsen','Elhan',13]
-users=[userA,userB]
+userB = ['Melike','Ahsen','Elhan',13]
+userC = ['Derin','ELhan',-15]
+users=[userA,userB,userC]
 #users dizisinin 1. elemanının 0. öğesini ekrana yazdır.
 print(users[1][0])
+print(len(userA+userB+userC))
